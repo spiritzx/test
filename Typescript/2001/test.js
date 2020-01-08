a91365d0ce6f1f8738b0e9fa9636f9d18f550c49
tomz <spirit108@foxmail.com>
2020-01-08 20:45:34 +0800

    typescript

---
+++ Typescript/2001/test.js
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion:
+ * @Author: tom-z(spirit108@foxmail.com)
+ * @Date: 2020-01-08 20:18:22
+ * @LastEditors  : tom-z(spirit108@foxmail.com)
+ * @LastEditTime : 2020-01-08 20:20:08
+ */
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Jane User";
+document.body.innerHTML = greeter(user);
